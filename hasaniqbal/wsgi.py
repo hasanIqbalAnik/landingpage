4ffc5f6226f7a062b94f8f488787812548893c0e
Hasan Iqbal <hasan.iqbal.anik@gmail.com>
2017-06-11 11:30:23 -0500

    css works in production thanks to https://github.com/kennethreitz/dj-static

--- hasaniqbal/wsgi.py
+++ hasaniqbal/wsgi.py
@@ -12,6 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling, MediaCling
 
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hasaniqbal.settings")
 
 
